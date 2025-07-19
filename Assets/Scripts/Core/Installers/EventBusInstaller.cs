6b73226b900fd76dcee12852e6815b6e2d7a252d
GregoryLukianov <76429464+GregoryLukianov@users.noreply.github.com>
2025-07-19 21:07:35 +0300

    event bus

---
+++ Assets/Scripts/Core/Installers/EventBusInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Core.Events;
+using Zenject;
+
+namespace Core.Installers
+{
+    public class EventBusInstaller: MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<EventBus>().AsSingle();
+        }
+    }
+}
